2a94bbd75facbdcb6fe29fb59676e3a10428c3b9
Cory Cowden <corycowden@Corys-MacBook-Air.local>
2025-08-01 16:20:59 -0700

    added reportIssue.ts

---
+++ frontend/src/grpc/reportIssue.ts
@@ -0,0 +1,45 @@
+import { issueReporterClient } from "./IssueReportServiceClientPb";
+import { issueRequest } from "./issueReport_pb";
+
+const client = new issueReporterClient("http://localhost:8080", null, null);
+
+export function reportIssue({
+  userEmail,
+  userDescription,
+  category,
+  subcategory,
+  urgent,
+  timestamp,
+  location,
+  motorData,
+}: {
+  userEmail: string;
+  userDescription: string;
+  category: string;
+  subcategory: string;
+  urgent: boolean;
+  timestamp: string;
+  location: string;
+  motorData: string;
+}) {
+  const request = new issueRequest();
+
+  request.setUseremail(userEmail);
+  request.setUserdescription(userDescription);
+  request.setCategory(category);
+  request.setSubcategory(subcategory);
+  request.setUrgent(urgent);
+  request.setTimestamp(timestamp);
+  request.setLocation(location);
+  request.setMotordata(motorData);
+
+  return new Promise((resolve, reject) => {
+    client.reportIssue(request, {}, (err, response) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(response?.getMessage());
+      }
+    });
+  });
+}
